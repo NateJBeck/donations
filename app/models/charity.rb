4392cc70f2ecf549bc83d9b3bdfb246ed0604d75
Nathan Beck <nathan.beck10@gmail.com>
2014-11-26 14:19:53 -0500

    Admins can see tomorrow, this week and month availabilities

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -5,4 +5,16 @@ class Charity < ActiveRecord::Base
 
   has_many :presences
   has_many :towns, through: :presences
+
+  def tomorrows_availabilities
+    availabilities.where(date: Date.tomorrow)
+  end
+
+  def upcoming_availabilities
+    availabilities.where(date: [Date.tomorrow .. Date.current+7]).group_by(&:date)
+  end
+
+  def availability_calendar
+    availabilities.group_by(&:date)
+  end
 end
